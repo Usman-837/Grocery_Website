9e48c9c7c8b9f8af281560631ab3938a62393b0b
Usman-837 <usmantufail837@gmail.com>
2024-06-22 22:27:45 +0500

    Add Responsive Navbar for Tab and Mobile Device and add some features in script file

--- js/script.js
+++ js/script.js
@@ -2,16 +2,41 @@
 let searchForm = document.querySelector(".search-form");
 document.querySelector(".Search").onclick = () => {
     searchForm.classList.toggle("active");
+    shoppingcart.classList.remove("active");
+    loginForm.classList.remove("active");
+    Navbar.classList.remove("active");
 }
 
 // Shopping cart
 let shoppingcart = document.querySelector(".shopping-cart");
 document.querySelector(".Cart").onclick = () => {
     shoppingcart.classList.toggle("active");
+    searchForm.classList.remove("active");
+    loginForm.classList.remove("active");
+    Navbar.classList.remove("active");
 }
 
 // Login Form
 let loginForm = document.querySelector(".login-form");
 document.querySelector(".User").onclick = () => {
     loginForm.classList.toggle("active");
+    shoppingcart.classList.remove("active");
+    searchForm.classList.remove("active");
+    Navbar.classList.remove("active");
+}
+
+// Navbar
+let Navbar = document.querySelector(".navbar");
+document.querySelector(".Bar").onclick = () => {
+    Navbar.classList.toggle("active");
+    loginForm.classList.remove("active");
+    shoppingcart.classList.remove("active");
+    searchForm.classList.remove("active");
+}
+
+window.onscroll = () => {
+    searchForm.classList.remove("active");
+    shoppingcart.classList.remove("active");
+    loginForm.classList.remove("active");
+    Navbar.classList.remove("active");
 }
