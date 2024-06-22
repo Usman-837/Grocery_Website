52bba01b38f93a42ccb13ae55bd1245da6a34765
Usman-837 <usmantufail837@gmail.com>
2024-06-22 15:14:09 +0500

    Add Login Form in Header Section

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
+// Search Bar
 let searchForm = document.querySelector(".search-form");
-
 document.querySelector(".Search").onclick = () => {
     searchForm.classList.toggle("active");
 }
 
+// Shopping cart
 let shoppingcart = document.querySelector(".shopping-cart");
-
-document.querySelector(".cart").onclick = () => {
+document.querySelector(".Cart").onclick = () => {
     shoppingcart.classList.toggle("active");
+}
+
+// Login Form
+let loginForm = document.querySelector(".login-form");
+document.querySelector(".User").onclick = () => {
+    loginForm.classList.toggle("active");
 }
