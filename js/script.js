184140480cd0f3992d223aee4ba5ece6bfb9693b
Usman-837 <usmantufail837@gmail.com>
2024-06-22 12:39:16 +0500

    Add Cart in Header Section

--- js/script.js
+++ js/script.js
@@ -3,3 +3,9 @@ let searchForm = document.querySelector(".search-form");
 document.querySelector(".Search").onclick = () => {
     searchForm.classList.toggle("active");
 }
+
+let shoppingcart = document.querySelector(".shopping-cart");
+
+document.querySelector(".cart").onclick = () => {
+    shoppingcart.classList.toggle("active");
+}
