7472330a6913117955c815e9ae59951724a7033e
Usman-837 <usmantufail837@gmail.com>
2024-06-26 14:38:49 +0500

    Add Review Section

--- js/script.js
+++ js/script.js
@@ -42,7 +42,31 @@ window.onscroll = () => {
 }
 
 
+// Product Slider
+
 var swiper = new Swiper(".products-slider", {
+    loop:true,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+    },
+
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1020: {
+        slidesPerView: 3,
+      },
+    },
+  });
+
+
+  var swiper = new Swiper(".review-slider", {
     loop:true,
     spaceBetween: 20,
     autoplay: {
